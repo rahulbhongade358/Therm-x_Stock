6245a790c05183e0ed03243841ccce4d2b851347
rahulbhongade358 <rahulbhongade912@gmail.com>
2025-10-03 20:33:14 +0530

    feat(client): initialize React app with Vite and Tailwind CSS

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="m-5">
+      <h1 className="font-semibold text-3xl text-blue-600">
+        Therm-X Stock Management
+      </h1>
+    </div>
+  );
+}
+
+export default App;
